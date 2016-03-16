f2c581980678aacce031334764dea66b9069d926
Scott <squayle45@gmail.com>
2016-03-16 13:09:11 +0200

    fourth commit

--- mainscript.js
+++ mainscript.js
@@ -2,11 +2,34 @@
   var xhr = new XMLHttpRequest();
   
   xhr.onreadystatechange = function() {
+    var getNodeValue;
     if (xhr.status === 200) {
-      document.getElementById('content').innerHTML = xhr.responseText;
+      var response = xhr.responseXML;
+      var events = response.getElementByTagName('event');
+      
+      for (var i = 0; i < events.length; i++) {
+        var container, location, city, newline;
+        
+        container = document.createElement('div');
+        container.className = 'event';
+        
+        location = document.createElement('p');
+        city = document.createElement('b');
+        newline = document.createElement('br');
+        city.appendChild(document.createTextNode(getNodeValue(events[i], 'location')));
+        location.appendChild(newline);
+        location.insertBefore(city, newline);
+        location.appendChild(document.createTextNode(getNodeValue(events[i], 'date')));
+        container.apendChild(location);
+        
+        document.getElementById('content').appendChild(container);
+        
+      }
+      getNodeValue = function(obj, tag) {
+        return obj.getElementsByTagName(tag)[0].firstChild.nodeValue;
+      }
     }
   };
-  
-  xhr.open('GET', 'data.html', true);
+  xhr.open('GET', 'data.xml', true);
   xhr.send(null);
 }());
