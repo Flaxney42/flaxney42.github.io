d30b2f4d46f0d4f022a7fffcaba028c635445935
scott <scott@southafrica.to>
2016-03-17 15:51:35 +0200

    Updated to contain code within  object

--- main-script.js
+++ main-script.js
@@ -1,8 +1,15 @@
-'use strict';
-$(':header').addClass('headline');
-$('li:lt(3)').hide().fadeIn(1500);
-$('li').on('click', function() {
-  $(this).remove();
+$(function() {
+  'use strict';
+  $(':header').addClass('headline');
+  $('li:lt(3)').hide().fadeIn(1500);
+  $('li').on('click', function() {
+    $(this).remove();
+  });
+  $('li em').addClass('seasonal');
+  $('li.hot').addClass('favorite');
+  $('li[id!="one"]')
+  .hide()
+  .delay(500)
+  .fadeIn(1400);
 });
-$('li em').addClass('seasonal');
-$('li.hot').addClass('favorite');
+
