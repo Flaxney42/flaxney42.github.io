39772e42194a7354b1459077116f3b9c60a30ba1
scott <scott@southafrica.to>
2016-03-22 15:06:48 +0200

    third commit

--- javascript/main-script.js
+++ javascript/main-script.js
@@ -1,6 +1,7 @@
 (function() {
   
   var htmlButton = document.getElementById("addHtmlBtn");
+  var xmlButton = document.getElementById("addXmlBtn");
   
   function requestHtmlAjax() {
     var xhr = new XMLHttpRequest();
@@ -14,5 +15,21 @@
     xhr.send(null);
   }
   
+  function requestXmlAjax() {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var response = xhr.responseXML;
+        var paragraphs = response.getElementsByTagName('p');
+        document.getElementById('xml').innerHTML = paragraphs;
+      }
+    };
+    
+    xhr.open('GET', '/xml-data/xml-data.xml', true);
+    xhr.send(null);
+   }
+  
   htmlButton.addEventListener("click", requestHtmlAjax);
+  
+  xmlButton.addEventListener("click", requestXmlAjax);
 }());
