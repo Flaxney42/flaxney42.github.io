c662d3aa72cb6dc2054cb7924ba22cf967b594fe
scott <scott@southafrica.to>
2016-03-23 09:29:27 +0200

    wah wah

--- javascript/main-script.js
+++ javascript/main-script.js
@@ -1,36 +1,51 @@
 (function() {
-  
-  var htmlButton = document.getElementById("addHtmlBtn");
-  
-  var jsonButton = document.getElementById("addJsonBtn");
-  
-  function requestHtmlAjax() { 
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        document.getElementById('html').innerHTML = xhr.responseText;
-      }
-    };
-    
-    xhr.open('GET', 'html-data.html', true);
-    xhr.send(null);
-  }
-  
-  function requestJsonAjax() {
-    var xhr = new XMLHttpRequest();
-    
-    if (xhr.status === 200) {
-      responseObject = JSON.parse(xhr.responseText);
-      var newContent = '';
-      newContent += responseObject["text"];
-      console.log(responseObject["text"]);
-      document.getElementById('json').innerHTML = newContent;
+    "use strict";
+
+    var htmlButton = document.getElementById("addHtmlBtn");
+
+    var jsonButton = document.getElementById("addJsonBtn");
+
+    function requestHtmlAjax() {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                document.getElementById('html').innerHTML = xhr.responseText;
+            }
+        };
+
+        xhr.open('GET', 'html-data.html', true);
+        xhr.send(null);
     }
-    xhr.open('GET', '/json-data/json-data.json', true);
-    xhr.send(null);
-  }
-  htmlButton.addEventListener("click", requestHtmlAjax);
-  
-  
-  jsonButton.addEventListener("click", requestJsonAjax)
-}());
+
+    function requestJsonAjax() {
+        var xhr = new XMLHttpRequest();
+
+        xhr.onload = function() {
+
+
+            if (xhr.status === 200) {
+                var responseObject = JSON.parse(xhr.responseText);
+                for (var i = 0; i < responseObject.restaurants.length; i ++) {
+                    document.getElementById('json').innerHTML += responseObject.restaurants[i].name +
+                    ", ";
+                }
+                
+            }
+        };
+
+        xhr.open('GET', '/json-data/json-data.json', true);
+        xhr.send(null);
+
+
+
+
+
+
+
+
+    }
+    htmlButton.addEventListener("click", requestHtmlAjax);
+
+
+    jsonButton.addEventListener("click", requestJsonAjax)
+} ());
